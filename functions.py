b160a2b62fd9391bc2c3edbb90d35f86581493f1
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-11-30 06:31:13 +0200

    Implement the Add Feature

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """
     Read a text file and return the list of
     to-do items
@@ -9,7 +11,7 @@ def get_todos(filepath="todos.txt"):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
