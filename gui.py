2e5fc6304e7f73e7028ab8c645c5790dd2ff2444
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-12-04 22:51:19 +0200

    Add the Exit Feature

--- gui.py
+++ gui.py
@@ -7,13 +7,15 @@ add_button = Sg.Button("Add")
 list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = Sg.Button("Edit")
+exit_button = Sg.Button("Exit")
 
 complete_button = Sg.Button("Complete")
 
 window = Sg.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button, complete_button]],
+                           [list_box, edit_button, complete_button],
+                            [exit_button]],
                    font=("Helvetica", 15))
 
 while True:
@@ -45,6 +47,8 @@ while True:
             functions.write_todos(todos)
             window["todo"].update(value='')
             window["todos"].update(values=todos)
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case Sg.WIN_CLOSED:
