d073d463db79f33208f6dcd9dd8cd544545bd70e
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-12-04 22:48:17 +0200

    Add the Complete Feature

--- gui.py
+++ gui.py
@@ -8,8 +8,12 @@ list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = Sg.Button("Edit")
 
+complete_button = Sg.Button("Complete")
+
 window = Sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button]],
                    font=("Helvetica", 15))
 
 while True:
@@ -33,6 +37,13 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
 
+            window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todo"].update(value='')
             window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
